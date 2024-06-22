15d6a6133f2200e43235fc97c663115ce68d5212
박도연 <87692596+parkdoyeon1@users.noreply.github.com>
2024-06-22 18:59:37 +0900

    [level 3] Title: 단속카메라, Time: 1.10 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,10 @@
+def solution(routes):
+    answer = 1
+    routes.sort(key = lambda x: x[1])
+    camera = routes[0][1]
+   
+    for i in range(len(routes)):
+        if camera < routes[i][0]:
+            camera = routes[i][1]
+            answer += 1
+    return answer
